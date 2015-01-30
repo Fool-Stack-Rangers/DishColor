87ee090f16f3959f709f2614b1d3d024aa567992
puff-tw <puff.tw+github@gmail.com>
2015-01-31 05:02:57 +0800

    GemFile changed

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,10 +1,9 @@
-set :stage, :production
-
 # Simple Role Syntax
 # ==================
 # Supports bulk-adding hosts to roles, the primary
 # server in each group is considered to be the first
 # unless any hosts have the primary property set.
+# Don't declare `role :all`, it's a meta role
 role :app, %w{deploy@example.com}
 role :web, %w{deploy@example.com}
 role :db,  %w{deploy@example.com}
@@ -38,5 +37,3 @@ server 'example.com', user: 'deploy', roles: %w{web app}, my_property: :my_value
 #     # password: 'please use keys'
 #   }
 # setting per server overrides global ssh_options
-
-# fetch(:default_env).merge!(rails_env: :production)
