0d7174bfd0908bae0fa0842833c3bf0a34a38744
Otis <otis@hdtech.com.tw>
2015-01-31 01:09:11 +0800

    landing pages init

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -5,4 +5,6 @@ Rails.application.config.assets.version = '1.0'
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-Rails.application.config.assets.precompile += %w( flat-ui/**/*  )
+Rails.application.config.assets.precompile += %w( flat-ui/**/* )
+Rails.application.config.assets.precompile += %w( landing.css )
+Rails.application.config.assets.precompile += %w( landing.js )
