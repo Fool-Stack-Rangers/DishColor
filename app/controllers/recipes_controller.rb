a2241341bad604407e28570cad66332f16d3f824
Daniel K. Lin <yettie.lin@gmail.com>
2015-01-31 22:05:35 +0800

    recipe_page

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,47 +1,55 @@
 class RecipesController < ApplicationController
-  before_action :set_recipe, only: [:show, :edit, :update, :destroy]
-
+  before_action :authenticate_user!, :only => [:index, :new, :create, :edit,:update,:destroy]
+  before_action :set_recipe, only: [:edit, :update, :destroy]
+  
   respond_to :html
 
   def index
-    @recipes = Recipe.all
+    @recipes = current_user.recipes.all
     respond_with(@recipes)
   end
 
   def show
-    respond_with(@recipe)
+    @recipe = Recipe.find(params[:id])
+    # respond_with(@recipe)
   end
 
   def new
-    @recipe = Recipe.new
-    respond_with(@recipe)
+    @recipe = current_user.recipes.new
+    respond_with(current_user,@recipe)
   end
 
   def edit
   end
 
   def create
-    @recipe = Recipe.new(recipe_params)
+    @user = current_user
+    @recipe = @user.recipes.build(recipe_params)
     flash[:notice] = 'Recipe was successfully created.' if @recipe.save
-    respond_with(@recipe)
+    respond_with(current_user,@recipe)
   end
 
   def update
     flash[:notice] = 'Recipe was successfully updated.' if @recipe.update(recipe_params)
-    respond_with(@recipe)
+    respond_with(current_user,@recipe)
   end
 
   def destroy
     @recipe.destroy
-    respond_with(@recipe)
+    respond_with(current_user,@recipe)
   end
 
   private
     def set_recipe
-      @recipe = Recipe.find(params[:id])
+       @user = current_user
+        if @user.recipes.find(params[:id]) != nil 
+          @recipe = @user.recipes.find(params[:id])        
+        else
+          redirect_to action: 'index'
+        end
     end
 
     def recipe_params
-      params.require(:recipe).permit(:name, :description, :user_id, :photo_id)
+      params.require(:recipe).permit(:id, :name, :description, :user_id, :image_id, :image)
     end
 end
--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,2 +1,10 @@
 class Recipe < ActiveRecord::Base
+  belongs_to :user
+
+  # paperclip: user can upload photos 
+
+  has_attached_file :image, :styles => { :medium => "200x200>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
+  validates_attachment_presence :image  
+  validates_attachment_size :image, :less_than => 5.megabytes  
+  validates_attachment_content_type :image, :content_type => %w(image/jpeg image/png)  
 end
